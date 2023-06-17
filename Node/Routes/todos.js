fa851cd7cca3cd3b9c03a020c59291e22883084e
Sebastian Acosta <eabastian@gmail.com>
2023-06-16 21:10:46 -0600

    Changed routes and html for pdf

--- Node/Routes/todos.js
+++ Node/Routes/todos.js
@@ -49,10 +49,11 @@ todos.get("/citas", async(req, res, next) =>{
     query += `UNION ALL SELECT para.idParasitologia, para.fecha, para.especie, para.nombre, p.nombre AS nombre_propietario, 'Parasitologia' AS tipo FROM parasitologia para
                 JOIN propietario p ON para.idpropietario = p.idPropietario WHERE para.completado IS NULL `;
     query += `UNION ALL SELECT u.idUrianalisis, u.fecha, u.especie, u.nombre, p.nombre AS nombre_propietario, 'Urianalisis' AS tipo FROM urianalisis u
-                JOIN propietario p ON u.idUrianalisis = p.idPropietario WHERE u.completado IS NULL `;
+                JOIN propietario p ON u.idPropietario = p.idPropietario WHERE u.completado IS NULL `;
     query += 'ORDER BY fecha ASC;';
 
     const rows = await db.query(query);
+    console.log(query)
 
     if(rows.length > 0){
         return res.status(200).json({code: 200, message: rows});
